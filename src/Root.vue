ef8bcf22a3688021c446dccf0f1665d2aef4d8fb
ONION-ZX <zxonion@gmail.com>
2018-07-21 22:55:01 +0800

    v1

---
+++ src/Root.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <!-- <Location/> -->
+  </div>
+</template>
+
+<script>
+  import Location from './components/Location';
+
+  export default {
+    name       : 'root',
+    components : {
+      Location,
+    },
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="footer">
+        <div>©2018 ONION • SPACE</div>
+    </div>
+</template>
+
+<style scoped>
+    .footer {
+        text-align: center;
+        margin: 50px 0 15px 0 ;
+        color:dimgray;
+        font-size: 13px;
+    }
+</style>
