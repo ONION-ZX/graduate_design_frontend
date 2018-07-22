a2faa8d82d5d3c435f7e3f947e396f5906463e95
ONION-ZX <zxonion@gmail.com>
2018-07-23 01:00:23 +0800

    update

--- src/page/Publish.vue
+++ src/page/Publish.vue
@@ -1,169 +1,147 @@
 <template>
-  <div>
-    <Nav/>
     <div class="header">
-      <div class="container por">
-        <form @submit="submit" class="main-form" autocomplete="off">
-          <div v-if="step == 1">
-            <h1>要卖什么车？</h1>
-            <div class="input-control">
-                <div class="brand">
-                    <label>品牌</label>
-                    <Dropdown :list="brand_list"
-                            :onSelect="set_brand_id"/>
+        <Nav/>
+        <div class="container">
+            <form @submit="submit" class="main-form">
+                <div v-if="step==1">
+                    <h1>要卖什么车?</h1>
+                    <div class="input-control">
+                        <label>品牌</label>
+                        <Dropdown :list="brand_list"
+                                :onSelect="set_brand_id"/>               
+                    </div>
+                    <div class="input-control">
+                        <label>型号</label>
+                        <Dropdown :list="model_list"
+                          :onSelect="set_model_id"/>               
+                    </div>
+                    <div class="input-control">
+                        <label>设计</label>
+                        <Dropdown :list="design_list"
+                                :onSelect="set_design_id"/>               
+                    </div>
                 </div>
-            </div>
-            <div class="input-control">
-                <div class="model">
-                    <label>型号</label>
-                    <Dropdown :list="model_list"
-                          :onSelect="set_model_id"/>
+                <div v-if="step==2">
+                    <input v-validator="required|telephone"
+                           v-model="current.phone"
+                           el-error="#phone-error"
+                           type="text"
+                           placeholder="请输入您的手机号码">
                 </div>
-            </div>
-          </div>
-
-          <div v-if="step == 2">
-            <h1>怎么联系你？</h1>
-            <div class="input-control">
-              <input v-validator="'required|telephone'"
-                     error-el="#phone-error"
-                     v-model="current.phone"
-                     :key="'telephone'"
-                     type="text"
-                     placeholder="请输入您的手机号">
-              <div class="error-list">
-                <div id="phone-error"></div>
-              </div>
-            </div>
-          </div>
-
-          <div v-if="step == 3">
-            <h1>发布成功</h1>
-            <p>审核中...</p>
-            <p>
-              <router-link to="/">回到首页</router-link>
-            </p>
-          </div>
-
-          <div class="row bottom">
-            <div class="col-lg-6">
-              <button type="button" @click="dec()" v-if="step > 1 && step < 3">上一步</button>
-            </div>
-            <div class="col-lg-6">
-              <button type="button" @click="inc()" v-if="step < 2">下一步</button>
-              <button type="submit" class="btn-primary" v-if="step == 2"
+                <div v-if="step==3">                   
+                    <h1>发布成功</h1>
+                    <p>审核中...</p>
+                    <router-link to="/">back to home</router-link>
+                </div>
+                <div class="btn-grop">
+                    <button type="button" @click="prev()" v-if="step > 1 && step < 3">上一步</button>
+                    <button type="button" @click="next()" v-if="step < 2">下一步</button>
+                    <button type="submit" class="btn-primary" v-if="step == 2"
                       :disabled="!current.brand_id || !current.model_id || !current.phone">发布二手车
-              </button>
-            </div>
-          </div>
-
-          <button class="btn-primary" type="submit" hidden>注册</button>
-        </form>
-      </div>
+                    </button>
+                </div>
+                <div class="btn-primary" type="submit" hidden></div>
+            </form>
+        </div>
+      <Footer/>
     </div>
-    <Footer/>
-  </div>
 </template>
 
 <script>
-  import Nav      from '../components/Nav';
-  import Footer      from '../components/Footer';
+import api from '../lib/api';
+import faker from '../lib/faker';
 
-  import Dropdown from '../components/Dropdown';
-  import api      from '../lib/api';
-  import session  from '../lib/session';
-  import faker    from '../lib/faker';
+import session  from '../lib/session';
 
-  export default {
-    mounted () {
-      this.list_brand();
-      this.list_model();
-      this.list_design();
+import Dropdown from '../components/Dropdown';
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+
+
+export default {
+    components: { Nav, Dropdown, Footer},
+    mounted() {
+        this.list_brand();
+        this.list_model();
+        this.list_design();
     },
-    data () {
-      return {
-        current     : {},
-        step        : 1,
-        brand_list  : [],
-        model_list  : [],
-        design_list : [],
-      };
+    data() {
+        return {
+            step: 1,
+            current: {},
+            brand_list: [],
+            model_list: [],
+            design_list: [],
+        };
     },
-    components : { Nav, Dropdown, Footer },
+    methods: {
+        prev() {
+            if(this.step >= 3 )
+                return;
+            this.step--;
+        },
+        next() {
+            if(this.step <= 1)
+                return;
+            this.step++;
+        },
+        list_brand() {
+            api('brand/read')
+               .then(r => {
+                   this.brand_list = r.data;
+               });
+        },
+        list_model() {
+            api('model/read')
+               .then(r => {
+                   this.model_list = r.data;
+               });
+        },
+        list_design() {
+            api('design/read')
+               .then(r => {
+                   this.design_list = r.data;
+               });
+        },
+        set_brand_id(row) {
+            this.$set(this.current, 'brand_id', row.id);
+        },
+        set_model_id(row) {
+            this.$set(this.current, 'model_id', row.id);
+        },
+        set_design_id(row) {
+            this.$set(this.current, 'design_id', row.id);
+        },
+        submit(e) {
+            e.preventDeault();
+            let fake = {};
+            let current = this.current;
+            let brand = this.brand_list.find((row) => {
+                return row.id == this.current.brand_id;
+            }).name;
+            let model = this.model_list.find((row) => {
+                return row.id == this.current.model_id;
+            }).name;
 
-    methods : {
-      inc () {
-        if (this.step >= 3)
-          return;
-        this.step++;
-      },
-      dec () {
-        if (this.step <= 1)
-          return;
-        this.step--;
-      },
-      submit (e) {
-        e.preventDefault();
-        let fake    = {};
-        let current = this.current;
+            current.publisher_id = session.his_id();
 
-        let brand = this.brand_list.find(it => {
-          return it.id == this.current.brand_id;
-        }).name;
+            fake.title = faker.title(brand, model);
+            fake.price = faker.price();
+            fake.publish_reason = faker.reason();
+            fake.exchange_times = faker.rand_int(1,5);
+            fake.birth_day = faker.rand_date();
+            fake.condition = faker.rand_int();
+            fake.design_id = this.design_list[faker.rand_int(0, this.design_list.length -1)].id;
 
-        let model = this.model_list.find(it => {
-          return it.id == this.current.model_id;
-        }).name;
+            current = Object.assign({}, current, fake);
+            api('vehicle/create', current)
+                .then(()=> {
+                    this.step == 3;
+                });
+        }
+    }
 
-
-        fake.title          = faker.title(brand, model);
-        fake.price          = faker.price();
-        fake.publish_reason = faker.reason();
-        fake.exchange_times = faker.rand_int(1, 5);
-        fake.birth_day      = faker.rand_date();
-        fake.deadline       = faker.rand_deadline();
-        fake.condition      = faker.rand_int(1, 9);
-        fake.design_id      = this.design_list[ faker.rand_int(0, this.design_list.length - 1) ].id;
-
-        current.publisher_id = session.his_id();
-
-        current = Object.assign({}, current, fake);
-
-        api('vehicle/create', current)
-          .then(() => {
-            this.step = 3;
-          });
-      },
-      list_brand () {
-        api('brand/read')
-          .then(r => {
-            this.brand_list = r.data;
-          });
-      },
-
-      list_design () {
-        api('design/read')
-          .then(r => {
-            this.design_list = r.data;
-          });
-      },
-
-      list_model () {
-        api('model/read')
-          .then(r => {
-            this.model_list = r.data;
-          });
-      },
-
-      set_model_id (row) {
-        this.$set(this.current, 'model_id', row.id);
-      },
-
-      set_brand_id (row) {
-        this.$set(this.current, 'brand_id', row.id);
-      },
-    },
-  };
+}
 </script>
 
 <style scoped>
@@ -196,6 +174,15 @@
 
   .bottom {
       margin-top: 20px;
+  }
+
+  .btn-group {
+      margin-bottom: 10px;
+  }
+
+  .btn-primary {
+      padding: 5px;
+      font-size: 15px;
   }
 
 </style>
